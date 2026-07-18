5d55d0e656d8f44bb497f13f5b348df5f3c2267d
D. Sunshine <david@sunshines.org>
2026-07-18 04:54:46 -0400

    Completed first listing

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Raytracing
+
+## Progress thus far
+- [X] [Listing 1](https://raytracing.github.io/books/RayTracingInOneWeekend.html#outputanimage)
+
+Reference: [Ray Tracing in One Weekend](https://raytracing.github.io/)
--- main.c
+++ main.c
@@ -3,20 +3,39 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+// Image
+int image_width  = 256;
+int image_height = 256;
+
 int main(void)
 {
-    InitWindow(400, 400, "Hello World");
+        InitWindow(image_width, image_height, "Hello World");
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+        while (!WindowShouldClose()) {
+                BeginDrawing();
+                /* ClearBackground(RAYWHITE); */
 
+                for (int j = 0; j < image_height; j++) {
+                        for (int i = 0; i < image_width; i++) {
+                                double r = ((double) i) / ((double) image_width  - 1);
+                                double g = ((double) j) / ((double) image_height - 1);
+                                double b = 0.0;
 
-        EndDrawing();
-    }
+                                Color c = (Color) { (unsigned char)(255.999 * r),
+                                                    (unsigned char)(255.999 * g),
+                                                    (unsigned char)(255.999 * b),
+                                                    255 };
+                        
+                                DrawPixel(i, j, c);
+                        }
 
-    CloseWindow();
+                }
+                /* asm("int3"); */
 
-    return 0;
+                EndDrawing();
+        }
+
+        CloseWindow();
+
+        return 0;
 }
